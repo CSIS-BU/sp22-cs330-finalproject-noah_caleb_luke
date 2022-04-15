4806d1918c238f167747b3d98281be3947dc7509
lukealcazar04 <62022740+lukealcazar04@users.noreply.github.com>
2022-04-15 17:49:59 -0500

    Skeleton Code*
    Put in two files that are based on the solutions he gave us to project one. Felt like this could be a great starting point to build off of.

---
+++ Player.c
@@ -0,0 +1,73 @@
+/*  This is based on the client file that he gave us after project one felt like we can use this
+    as our basic scaffolding. Then build more off of it
+*/
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <netdb.h> 
+#include <string.h> 
+#include <unistd.h> 
+#include <stdlib.h> 
+
+#define MAX_SIZE 2048 
+ 
+int 
+main(int argc, char * argv[]) 
+{ 
+    FILE *fp; 
+    struct hostent *hp; 
+    struct sockaddr_in sin; 
+    char *host, *port; 
+    char buf[MAX_SIZE]; 
+    int s; 
+    int len; 
+ 
+    if (argc==3) { 
+        host = argv[1]; 
+        port = argv[2]; 
+    } 
+    else { 
+        fprintf(stderr, "usage: %s host port\n", argv[0]); 
+        exit(1); 
+    } 
+ 
+
+    hp = gethostbyname(host); 
+    if (!hp) { 
+        fprintf(stderr, "%s: unknown host\n", host); 
+        exit(1); 
+    } 
+ 
+
+    bzero((char *)&sin, sizeof(sin)); 
+    sin.sin_family = AF_INET; 
+    bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length); 
+    sin.sin_port = htons(atoi(port)); 
+
+    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) { 
+        perror("simplex-talk: socket"); 
+        exit(1); 
+    } 
+    if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0) 
+    { 
+        perror("simplex-talk: connect"); 
+        close(s); 
+        exit(1); 
+    } 
+ 
+    int size; 
+    while( (size=fread(buf, 1, MAX_SIZE, stdin) )) { 
+        if(send(s, buf, size, 0) < 0) { 
+            perror("client: send"); 
+        } 
+    } 
+ 
+    close(s); 
+} 
+ 
+ 
+ 
+ 
+ 
+ 
---
+++ Server.c
@@ -0,0 +1,68 @@
+/*  This is based on the server file that he gave us after project one felt like we can use this
+    as our basic scaffolding. Then build more off of it
+*/
+
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <netdb.h> 
+ 
+#include <strings.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+ 
+#define MAX_PENDING  5 
+#define MAX_SIZE 2048 
+ 
+int 
+main(int argc, char** argv) 
+{ 
+    struct sockaddr_in sin; 
+    char buf[MAX_SIZE]; 
+    int buf_len; 
+    socklen_t addr_len; 
+    int s, new_s; 
+    char *port; 
+ 
+    if(argc==2) { 
+        port = argv[1]; 
+    } 
+    else { 
+        fprintf(stderr, "usage: %s port\n", argv[0]); 
+        exit(1); 
+    } 
+ 
+    /* build address data structure */ 
+    bzero((char *)&sin, sizeof(sin)); 
+    sin.sin_family = AF_INET; 
+    sin.sin_addr.s_addr = INADDR_ANY; 
+    sin.sin_port = htons(atoi(port)); 
+ 
+    /* setup passive open */ 
+    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) { 
+        perror("simplex-talk: socket"); 
+        exit(1); 
+    } 
+    if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) { 
+        perror("simplex-talk: bind"); 
+        exit(1); 
+    } 
+    listen(s, MAX_PENDING); 
+ 
+    /* wait for connection, then receive and print text */ 
+    while(1) { 
+        if ((new_s = accept(s, (struct sockaddr *)&sin, &addr_len)) < 0) { 
+            fprintf(stderr, "error: %s accept\n", argv[0]); 
+            exit(1); 
+        } 
+        while ( (buf_len = recv(new_s, buf, sizeof(buf), 0)) ) { 
+            fwrite(buf, 1, buf_len, stdout); 
+        } 
+        fflush(stdout); 
+        close(new_s); 
+    } 
+    close(s); 
+ 
+    return 0; 
+} 
